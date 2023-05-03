7ea00993dd084d3a02e675546b64a697bb93f3e6
blue butter <damiongallarza@gmail.com>
2023-05-02 17:55:51 -0700

    final changes made to Java

--- Develop/script.js
+++ Develop/script.js
@@ -7,7 +7,7 @@ function generatePassword() {
   const special = "!@#$%^&*()_-{}[]+=|:;<>,.?";
   const numbers = "0123456789";
   // fullCriteria pulls properties from all 4 strings
-  // const fullCriteria = uppercase + lowercase + special + numbers;
+  let fullCriteria = "";
   // Prompt to allow user to select password char length
 
 
@@ -15,41 +15,48 @@ function generatePassword() {
   let password = "";
   console.log(passwordlength)
 
-  const passCrit = window.prompt("Would you like your password to include special characters?");
+  const passCrit = window.confirm("Would you like your password to include special characters?");
 
-  if (passCrit === "yes") {
-    password += special[getRandomIndex(special)]
+  if (passCrit) {
+    password += getRandomCharacter(special);
+    // fullCriteria = fullCriteria + special ;
+    fullCriteria += special;
   }
   console.log(passCrit)
-  function getRandomIndex(array) {
-    let randomIndex = Math.floor(Math.random() * array.length);
-    return randomIndex;
 
+  const includeNumbers = window.confirm("Would you like your password to include numbers?");
 
+  if (includeNumbers) {
+    password += getRandomCharacter(numbers);
+    fullCriteria += numbers;
   }
 
+  const includeLower = window.confirm("Would you like your password to include lower case letters?");
+
+  if (includeLower) {
+    password += getRandomCharacter(lowercase);
+    fullCriteria += lowercase;
+
+  }
+  const includeUpper = window.confirm("Would you like your password to include uppercase letters?");
+
+  if (includeUpper) {
+    password += getRandomCharacter(uppercase);
+    fullCriteria += uppercase
+  }
+  while (password.length < passwordlength) {
+    password += getRandomCharacter(fullCriteria)
+  }
+
+  return password;
 
 
-
-
-const passrit = window.prompt("Would you like your password to include numbers");
 }
 
-// for (let i = 0; i < passwordlength; i++) {
-//   let  randomIndex = Math.floor(Math.random() * fullCriteria.length);
-//   password += fullCriteria[randomIndex];
-// }
-
-// return password;
-// }
-
-const newPassword = generatePassword();
-alert(`Your new password is: ${newPassword}`);
-
-document.getElementById("placeholder").value = password;
-
-
-
+function getRandomCharacter(str) {
+  let randomIndex = Math.floor(Math.random() * str.length);
+  return str[randomIndex];
+}
 
 // Write password to the #password input
 function writePassword() {
