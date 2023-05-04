b1c0c9d58666956b1c31eb4917c1062b0b6c77aa
blue butter <damiongallarza@gmail.com>
2023-05-03 20:25:56 -0700

    Final changes and comments added to Java

--- Develop/script.js
+++ Develop/script.js
@@ -6,31 +6,26 @@ function generatePassword() {
   const lowercase = "abcdefghijklmnopqrstuvwxyz";
   const special = "!@#$%^&*()_-{}[]+=|:;<>,.?";
   const numbers = "0123456789";
-  // fullCriteria pulls properties from all 4 strings
+  // fullCriteria pulls strings based off of users answers .
   let fullCriteria = "";
   // Prompt to allow user to select password char length
-
-
   const passwordlength = window.prompt("Choose  password length (8-128 characters). ");
   let password = "";
   console.log(passwordlength)
-
+  // Prompt for password criteria to include 
   const passCrit = window.confirm("Would you like your password to include special characters?");
 
   if (passCrit) {
     password += getRandomCharacter(special);
-    // fullCriteria = fullCriteria + special ;
     fullCriteria += special;
   }
   console.log(passCrit)
-
   const includeNumbers = window.confirm("Would you like your password to include numbers?");
-
+// determines what strings to pull from for password
   if (includeNumbers) {
     password += getRandomCharacter(numbers);
     fullCriteria += numbers;
   }
-
   const includeLower = window.confirm("Would you like your password to include lower case letters?");
 
   if (includeLower) {
@@ -47,17 +42,12 @@ function generatePassword() {
   while (password.length < passwordlength) {
     password += getRandomCharacter(fullCriteria)
   }
-
   return password;
-
-
 }
-
 function getRandomCharacter(str) {
   let randomIndex = Math.floor(Math.random() * str.length);
   return str[randomIndex];
 }
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
